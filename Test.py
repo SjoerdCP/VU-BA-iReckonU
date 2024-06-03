4bcce8757fa9ddb7a783091ea7831134fdefff35
Lex Holzhauer <l.a.holzhauer@student.vu.nl>
2024-06-03 11:09:01 +0200

    Update

--- Test.py
+++ Test.py
@@ -3,8 +3,12 @@ import os
 import numpy as np
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import load_model
+from LaplaceModel import LaplaceModel
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix, fbeta_score, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 from EvaluateModel import evaluate_model
+from PreprocessData import preprocess_data
 
 # # Replace 'your_excel_file.xlsx' with the path to your Excel file
 # file_path = './PublicDataset/Evaluate/NaturalBlurSet.xlsx'
@@ -74,6 +78,46 @@ y_test = np.array([label_to_index[label] for label in y_test])
 
 y_test = y_test.reshape(-1, 1)
 
-model = load_model('MaxResNet50.keras')
+model = load_model('TestResNet.keras')
 
 evaluate_model(model, X_test, y_test)
+
+# model = LaplaceModel()
+# model.set_threshold(0.01890320665502987)
+
+# X_test = preprocess_data(X_test)
+# y_pred = model.predict(X_test)
+
+# # Calculate the required metrics
+# accuracy = accuracy_score(y_test, y_pred)
+# precision = precision_score(y_test, y_pred)
+# recall = recall_score(y_test, y_pred)
+# f1 = f1_score(y_test, y_pred)
+# #roc_auc = roc_auc_score(y_test, y_pred_prob)  # Use probabilities for ROC AUC
+# conf_matrix = confusion_matrix(y_test, y_pred, labels = [0, 1])
+# fbeta = fbeta_score(y_test, y_pred, beta = 0.5)
+
+# # Printing the metrics
+# print(f'Accuracy: {accuracy}')
+# print(f'Precision: {precision}')
+# print(f'Recall: {recall}')
+# print(f'F1 Score: {f1}')
+# print(f'F0.5 Score: {fbeta}')
+# #print(f'ROC-AUC Score: {roc_auc}')
+# print(f'Confusion Matrix:\n{conf_matrix}')
+
+# print(model.threshold)
+# print(y_pred)
+
+# disp = ConfusionMatrixDisplay(confusion_matrix=conf_matrix, display_labels=['Sharp', 'Blurred'])
+# disp.plot(cmap = 'Reds', colorbar = False, text_kw = {'fontsize': 12})
+
+# ax = plt.gca()
+
+# # Optionally, you can also increase the font size of the axis labels
+# ax.set_xlabel('Predicted label', fontsize=14)
+# ax.set_ylabel('True label', fontsize=14)
+# ax.set_xticklabels(['Sharp', 'Blurred'], fontsize=12)
+# ax.set_yticklabels(['Sharp', 'Blurred'], fontsize=12)
+
+# plt.show()
